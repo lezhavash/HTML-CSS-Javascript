d17fbd5e8fe08d440c26bb046f7428c6b6b561ed
Shota Lezhava <s.lezhava@imedi.ge>
2024-02-13 23:18:20 +0400

    Chalang 1

--- javascript/Fundaments-1/script.js
+++ javascript/Fundaments-1/script.js
@@ -1,3 +1,20 @@
-let js = "amaizing";
-if (js === "amaizing") alert("test");
-console.log(5 + 4);
+let marksWeightData1 = 78;
+let marksTallData1 = 1.69;
+let johnWeightData1 = 92;
+let johnTallData1 = 1.95;
+
+let marksBMIData1 = marksWeightData1 / marksTallData1 ** 2;
+let johnBMIData1 = johnWeightData1 / johnTallData1 ** 2;
+let markHigherBMIData1 = marksBMIData1 > johnBMIData1;
+
+let marksWeightData2 = 95;
+let marksTallData2 = 1.88;
+let johnWeightData2 = 85;
+let johnTallData2 = 1.76;
+
+let marksBMIData2 = marksWeightData2 / marksTallData2 ** 2;
+let johnBMIData2 = johnWeightData2 / johnTallData2 ** 2;
+let markHigherBMIData2 = marksBMIData2 > johnBMIData2;
+
+console.log(markHigherBMIData1);
+console.log(markHigherBMIData2);
