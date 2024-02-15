932f6d2cb18d7bbdd7a332f076aaef16dc839f3d
Shota Lezhava <lezhavash@gmail.com>
2024-02-15 18:12:10 +0400

    Functions

--- javascript/Fundaments-1/script.js
+++ javascript/Fundaments-1/script.js
@@ -1,3 +1,5 @@
+`use strict`;
+
 // let marksWeightData1 = 78;
 // let marksTallData1 = 1.69;
 // let johnWeightData1 = 92;
@@ -20,31 +22,57 @@
 //   console.log(`johns ${johnBMIData1} is more then marks`);
 // }
 
-const delphineScore1 = 100;
-const delphineScore2 = 100;
-const delphineScore3 = 100;
-const delphineAverage = (delphineScore1 + delphineScore2 + delphineScore3) / 3;
+// const delphineScore1 = 100;
+// const delphineScore2 = 100;
+// const delphineScore3 = 100;
+// const delphineAverage = (delphineScore1 + delphineScore2 + delphineScore3) / 3;
 
-const koalaScore1 = 100;
-const koalaScore2 = 100;
-const koalaScore3 = 100;
-const koalaAverage = (koalaScore1 + koalaScore2 + koalaScore3) / 3;
+// const koalaScore1 = 100;
+// const koalaScore2 = 100;
+// const koalaScore3 = 100;
+// const koalaAverage = (koalaScore1 + koalaScore2 + koalaScore3) / 3;
 
-if (delphineAverage >= 100 || koalaAverage >= 100) {
-  if (delphineAverage > koalaAverage) {
-    console.log(`Delphine team winnes`);
-  } else if (delphineAverage < koalaAverage) {
-    console.log(`Koala team winnes`);
-  } else if (
-    delphineAverage === koalaAverage &&
-    delphineAverage >= 100 &&
-    koalaAverage >= 100
+// if (delphineAverage >= 100 || koalaAverage >= 100) {
+//   if (delphineAverage > koalaAverage) {
+//     console.log(`Delphine team winnes`);
+//   } else if (delphineAverage < koalaAverage) {
+//     console.log(`Koala team winnes`);
+//   } else if (
+//     delphineAverage === koalaAverage &&
+//     delphineAverage >= 100 &&
+//     koalaAverage >= 100
+//   ) {
+//     console.log(`Both winn s`);
+//   }
+// } else {
+//   console.log(`Nowan winns`);
+// }
+
+// let height;
+// console.log(typeof height);
+
+// let bill = 430;
+// let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// console.log(`The bill was ${bill}, the tip ${tip}, and total ${bill + tip}`);
+
+let calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+let delphineAverage = calcAverage(20, 20, 20);
+let koalaAverage = calcAverage(30, 30, 30);
+
+function checkWinner(delphineAverage, koalaAverage) {
+  if (
+    delphineAverage / 2 >= koalaAverage ||
+    koalaAverage / 2 >= delphineAverage
   ) {
-    console.log(`Both winn s`);
+    if (delphineAverage > koalaAverage) {
+      console.log(`Delphines win`);
+    } else if (koalaAverage > delphineAverage) {
+      console.log(`Koalas win`);
+    }
+  } else {
+    console.log(`Loose `);
   }
-} else {
-  console.log(`Nowan winns`);
 }
 
-let height;
-console.log(typeof height);
+checkWinner(delphineAverage, koalaAverage);
