1c997e3b2c6806d4280769ee4a21f3a2ea021527
Shota Lezhava <lezhavash@gmail.com>
2024-02-16 19:32:44 +0400

    Fundamets

--- javascript/Fundaments-1/script.js
+++ javascript/Fundaments-1/script.js
@@ -55,24 +55,97 @@
 // let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 // console.log(`The bill was ${bill}, the tip ${tip}, and total ${bill + tip}`);
 
-let calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// let calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-let delphineAverage = calcAverage(20, 20, 20);
-let koalaAverage = calcAverage(30, 30, 30);
+// let delphineAverage = calcAverage(20, 20, 20);
+// let koalaAverage = calcAverage(30, 30, 30);
 
-function checkWinner(delphineAverage, koalaAverage) {
-  if (
-    delphineAverage / 2 >= koalaAverage ||
-    koalaAverage / 2 >= delphineAverage
-  ) {
-    if (delphineAverage > koalaAverage) {
-      console.log(`Delphines win`);
-    } else if (koalaAverage > delphineAverage) {
-      console.log(`Koalas win`);
-    }
+// function checkWinner(delphineAverage, koalaAverage) {
+//   if (
+//     delphineAverage / 2 >= koalaAverage ||
+//     koalaAverage / 2 >= delphineAverage
+//   ) {
+//     if (delphineAverage > koalaAverage) {
+//       console.log(`Delphines win`);
+//     } else if (koalaAverage > delphineAverage) {
+//       console.log(`Koalas win`);
+//     }
+//   } else {
+//     console.log(`Loose `);
+//   }
+// }
+
+// checkWinner(delphineAverage, koalaAverage);
+
+// let bills = [125, 555, 44];
+// let calcTip = function (bill) {
+//   if (bill >= 50 && bill <= 300) {
+//     return bill * 0.15;
+//   } else {
+//     return bill * 0.2;
+//   }
+// };
+
+// let tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(tips);
+
+// let total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// console.log(total);
+
+// let mark = {
+//   fullName: "Mark Miller",
+//   mass: 79,
+//   height: 1.69,
+//   calcBMI: function () {
+//     this.bmi = this.mass / this.height ** 2;
+//     return this.bmi;
+//   },
+// };
+
+// let john = {
+//   fullName: "John Smith",
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function () {
+//     this.bmi = this.mass / this.height ** 2;
+//     return this.bmi;
+//   },
+// };
+
+// if (mark.calcBMI() > john.calcBMI()) {
+//   console.log(
+//     `${mark.fullName}'s BMI (${mark.bmi}) is higher then ${john.fullName}'s  (${john.bmi}) `
+//   );
+// } else {
+//   console.log(
+//     `${john.fullName}'s BMI (${john.bmi}) is higher then ${mark.fullName}'s  (${mark.bmi}) `
+//   );
+// }
+
+let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+let total = [];
+let calcTip = function (bill) {
+  if (bill >= 50 && bill <= 300) {
+    tips.push(bill * 0.15);
   } else {
-    console.log(`Loose `);
+    tips.push(bill * 0.2);
   }
+};
+
+for (let i = 0; i < bills.length; i++) {
+  calcTip(bills[i]);
+  total[i] = bills[i] + tips[i];
 }
 
-checkWinner(delphineAverage, koalaAverage);
+console.log(bills, tips, total);
+
+let calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+
+console.log(calcAverage(total));
