de4cec8274dedea86b90b600f35c7efe7e39e020
Shota Lezhava <lezhavash@gmail.com>
2024-02-28 15:02:35 +0400

    Start Model

--- javascript/Guss-Number/script.js
+++ javascript/Guss-Number/script.js
@@ -1,23 +1,23 @@
 "use strict";
-// document.querySelector(".message").textContent = "Corrent Number";
-// document.querySelector(".guess").value = "23";
-// console.log(document.querySelector(".guess").value);
-
 let newNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(newNumber);
 let score = 20;
 let highScore = 0;
 
+const displayMessig = function (messige) {
+  document.querySelector(".message").textContent = messige;
+};
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔️ No Number";
+    displayMessig("⛔️ No Number");
   }
 
   if (score > 1) {
     if (newNumber === guess) {
-      document.querySelector(".message").textContent = "✅ Currect Number";
+      displayMessig("✅ Currect Number");
       document.querySelector("body").style.backgroundColor = "#60b347";
       document.querySelector(".number").textContent = newNumber;
       if (score > highScore) {
@@ -25,16 +25,16 @@ document.querySelector(".check").addEventListener("click", function () {
         document.querySelector(".highscore").textContent = highScore;
       }
     } else if (guess > newNumber) {
-      document.querySelector(".message").textContent = "⬇️ Lower";
+      displayMessig("⬇️ Lower");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "⬆️ Upper";
+      displayMessig("⬆️ Upper");
       score--;
       document.querySelector(".score").textContent = score;
     }
   } else {
-    document.querySelector(".message").textContent = "🔂 You lose";
+    displayMessig("🔂 You lose");
     document.querySelector(".score").textContent = 0;
   }
 });
@@ -43,7 +43,7 @@ document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   document.querySelector(".score").textContent = 20;
   document.querySelector("body").style.backgroundColor = "#222";
-  document.querySelector(".message").textContent = "Start guessing...";
+  displayMessig("Start guessing...");
   document.querySelector(".guess").value = "?";
   newNumber = Math.trunc(Math.random() * 20) + 1;
   console.log(newNumber);
