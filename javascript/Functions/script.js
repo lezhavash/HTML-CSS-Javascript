47a55ba138aa381fa8d6eb8244184fb960de8373
Shota Lezhava <lezhavash@gmail.com>
2024-03-11 23:17:18 +0400

    Test

--- javascript/Functions/script.js
+++ javascript/Functions/script.js
@@ -1,1 +1,18 @@
-'use strict';
+"use strict";
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  // numPassengers ||= 1;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("ESD123");
