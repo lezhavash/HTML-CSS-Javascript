7e5a6c8e3aa5f635a074a9f627036f36a6bd048f
Shota Lezhava <lezhavash@gmail.com>
2024-03-12 13:35:27 +0400

    test

--- javascript/Functions/script.js
+++ javascript/Functions/script.js
@@ -1,18 +1,38 @@
 "use strict";
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (flightNum, numPassengers = 1, price = 199) {
-  // numPassengers ||= 1;
+// const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+//   // numPassengers ||= 1;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
 
-  console.log(booking);
-  bookings.push(booking);
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking("ESD123");
+
+const flight = "LSWS23";
+const jonas = {
+  name: "Jonas Sehesvs",
+  pasport: 343234542,
 };
 
-createBooking("ESD123");
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LES342";
+  passenger.name = "Mr " + passenger.name;
+  if (passenger.pasport === 343234542) {
+    console.log("CheckIN");
+  } else {
+    console.log("No");
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
