48ae6645af02565dbdde0049f558a94c8a2a081a
Shota Lezhava <lezhavash@gmail.com>
2024-03-12 15:28:39 +0400

    test

--- javascript/Functions/script.js
+++ javascript/Functions/script.js
@@ -17,22 +17,54 @@
 
 // createBooking("ESD123");
 
-const flight = "LSWS23";
-const jonas = {
-  name: "Jonas Sehesvs",
-  pasport: 343234542,
+//////
+
+// const flight = "LSWS23";
+// const jonas = {
+//   name: "Jonas Sehesvs",
+//   pasport: 343234542,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = "LES342";
+//   passenger.name = "Mr " + passenger.name;
+//   if (passenger.pasport === 343234542) {
+//     console.log("CheckIN");
+//   } else {
+//     console.log("No");
+//   }
+// };
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+////////
+
+// const oneword = function (str) {
+//   return str.replaceAll(" ", "").toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(" ");
+//   return [first.toUpperCase(), ...others].join(" ");
+// };
+
+// const transform = function (str, fn) {
+//   console.log(`Original string : ${str}`);
+//   console.log(`Transform string: ${fn(str)}`);
+// };
+
+// transform("Javascript is the best", upperFirstWord);
+// transform("Javascript is the best", oneword);
+
+///////
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
 };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = "LES342";
-  passenger.name = "Mr " + passenger.name;
-  if (passenger.pasport === 343234542) {
-    console.log("CheckIN");
-  } else {
-    console.log("No");
-  }
-};
-
-checkIn(flight, jonas);
-console.log(flight);
-console.log(jonas);
+const greeterHey = greet("Hey");
+greeterHey("Shota");
