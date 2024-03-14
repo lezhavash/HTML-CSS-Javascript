7abf2dbfd927876699dd0195a8e27e51fa418abb
Shota Lezhava <lezhavash@gmail.com>
2024-03-14 18:39:10 +0400

    test

--- javascript/Array/script.js
+++ javascript/Array/script.js
@@ -75,9 +75,31 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-let arr = ["a", "b", "c", "d", "e"];
+// let arr = ["a", "b", "c", "d", "e"];
 
-let a = arr.slice();
-a.push("m");
-console.log(arr);
-console.log(a);
+// let a = arr.slice();
+// a.push("m");
+// console.log(arr);
+// console.log(a);
+
+// a.splice(-1);
+// console.log(arr);
+//////////
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+
+    const html = `
+    <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__value">${mov}</div>
+    </div>`;
+
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+displayMovements(account1.movements);
