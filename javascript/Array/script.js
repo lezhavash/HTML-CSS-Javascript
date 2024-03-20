0eee56aed640ebfb2e290c1869cbe1acbcfa5d95
Shota Lezhava <lezhavash@gmail.com>
2024-03-20 17:39:36 +0400

    P4

--- javascript/Array/script.js
+++ javascript/Array/script.js
@@ -333,3 +333,10 @@ const convertTitleCase = function (title) {
 };
 
 console.log(convertTitleCase("and it is a nice in sadasd title"));
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
+  { weight: 8, curFood: 200, owners: ["Matilda"] },
+  { weight: 13, curFood: 275, owners: ["Sarah", "John"] },
+  { weight: 32, curFood: 340, owners: ["Michael"] },
+];
