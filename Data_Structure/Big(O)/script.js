3b990d6ad4d7fe2d0ea05d8ad81d0cf5f6dad0dc
Shota Lezhava <lezhavash@gmail.com>
2024-04-30 13:21:34 +0400

    Big Data

---
+++ Data_Structure/Big(O)/script.js
@@ -0,0 +1,11 @@
+const nemo = ["nemo"];
+
+function findNemp(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") {
+      console.log("Found Nemo!");
+    }
+  }
+}
+
+findNemp(nemo);
