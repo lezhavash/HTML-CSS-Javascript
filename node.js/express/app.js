5f74fb13b7bcf5eb38b36b831a9c936159814ae7
Shota Lezhava <lezhavash@gmail.com>
2025-01-09 18:29:55 +0400

    Test

--- node.js/express/app.js
+++ node.js/express/app.js
@@ -1,13 +1,107 @@
+const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello From the server', app: 'Natours!' });
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello form the middleware');
+  next();
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this enddpoint');
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'seccess',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+const getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'tour Not found',
+    });
+  }
+
+  res.status(200).json({
+    status: 'seccess',
+    data: {
+      tour,
+    },
+  });
+};
+
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        starus: 'seccess',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+const updateTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'seccess',
+    data: {
+      tour: 'Updated',
+    },
+  });
+};
+
+const delateTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  res.status(204).json({
+    status: 'seccess',
+    data: {
+      tour: 'Deleted',
+    },
+  });
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', delateTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(delateTour);
 
 const port = 3000;
 app.listen(port, () => {
