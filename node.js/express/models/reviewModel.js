6860c27d5caea59933de62678404532b8c0edcf3
Shota Lezhava <lezhavash@gmail.com>
2025-02-10 18:06:05 +0400

    test

--- node.js/express/models/reviewModel.js
+++ node.js/express/models/reviewModel.js
@@ -33,6 +33,8 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
@@ -54,7 +56,6 @@ reviewSchema.statics.calcAvarageRatings = async function (tourId) {
       },
     },
   ]);
-  console.log(stats);
 
   if (stats.length > 0) {
     await Tour.findByIdAndUpdate(tourId, {
--- node.js/express/models/tourModel.js
+++ node.js/express/models/tourModel.js
@@ -32,6 +32,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be about 1.0'],
       max: [5, 'Rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10,
     },
     ratingQuantity: {
       type: Number,
