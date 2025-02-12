f6b97d8879e15d7dfca701d25e5b7a0ffca5f511
Shota Lezhava <lezhavash@gmail.com>
2025-02-12 22:02:08 +0400

    test

--- node.js/express/routes/tourRouts.js
+++ node.js/express/routes/tourRouts.js
@@ -20,7 +20,11 @@ router
     tourController.getMonthlyPlan,
   );
 
-router.route('tours-within/:distance/center/:latlng/unit/:unit');
+router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
 
 router
   .route('/')
