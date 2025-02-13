b13e98ad573435347d024f516273c8c348676a34
Shota Lezhava <lezhavash@gmail.com>
2025-02-13 20:58:51 +0400

    test

--- node.js/express/app.js
+++ node.js/express/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
+
 const hpp = require('hpp');
 
 const AppError = require('./utils/appError.js');
@@ -32,8 +32,6 @@ app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
 
 app.use(mongoSanitize());
-
-app.use(xss());
 
 app.use(
   hpp({
